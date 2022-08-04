7387c9cb003eeb98a2ee8004434c6b932f32ceb1
Nicat Tahmazov <nijattahmazovv@gmail.com>
2022-08-04 18:20:18 +0400

    MethodTask

---
+++ ArrayinENKicikElementiMin/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayinENKicikElementiMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 14, 30, 8, 20, 39, 4 };
+            Console.WriteLine(Min(arr));
+        }
+        static int Min(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]<min)
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
+    }
+}
